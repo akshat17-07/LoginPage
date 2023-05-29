03d6fbe4fe10d3d4f05d7de4f8d490dc9dc945af
Akshat Harlalka <avh5936@psu.edu>
2023-05-29 07:15:06 -0400

    login created

---
+++ UiDevelopment/UiDevelopmentWeb/Controllers/LoginController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UiDevelopmentWeb.Models;
+
+namespace UiDevelopmentWeb.Controllers
+{
+    [ApiController]
+    public class LoginController : Controller
+    {
+        private readonly IConfiguration _User;
+        public LoginController(IConfiguration user)
+        {
+            _User = user;
+        }
+
+        [HttpGet]
+        [Route("login")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        [Route("registration")]
+        public ViewResult Registration()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("registration")]
+        public IActionResult Register(RegistrationViewModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ UiDevelopment/UiDevelopmentWeb/Models/RegistrationViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace UiDevelopmentWeb.Models
+{
+    public class RegistrationViewModel
+    {
+        [Required]
+        public string firstName { get; set; }
+
+        [Required]
+        public string lastName { get; set; }
+
+        public string gender { get; set; }
+
+        [Key]
+        [Required, MaxLength(256)]
+        public string email { get; set; }
+
+        [Required, DataType(DataType.Password)]
+        public string password { get; set; }
+
+        [DataType(DataType.Password), Compare(nameof(password))]
+        public string comfirmPassword { get; set; }
+
+        [Required]
+        public string city { get; set; }
+
+        [Required]
+        public string state { get; set; }
+
+        [Required]
+        public string zipcode { get; set; }
+
+        [Required]
+        public string country { get; set; }
+        [NotMapped]
+        public IFormFile profilePicture { get; set; }
+        public string intrest { get; set; }
+    }
+}
